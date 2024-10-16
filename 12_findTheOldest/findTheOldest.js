4a5c4a664372ea4a374473c522ff6630b7b7d9fa
Dkaffe <109029615+Dkaffe@users.noreply.github.com>
2024-10-16 10:33:20 +0200

    Finish last tests for JS intro

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, person) => {
+        // Handle if yearOfDeath is missing (person is still alive)
+        const currentYear = new Date().getFullYear();
+        const yearOfDeath = person.yearOfDeath || currentYear;
 
+        // Calculate the person & oldest age
+        const personAge = yearOfDeath - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        
+        return personAge > oldestAge ? person : oldest;
+    }, arr[0])
 };
 
 // Do not edit below this line
